3e7202bc6ee3d8ddcb98b442ee98ed53adfad8ba
chunhongZheng <824394795@qq.com>
2023-03-11 16:02:51 +0800

    校验包由javaee转换为Jakarta EE API

---
+++ eservice-third-party/src/main/java/com/caspar/eservicemall/thirdparty/EserviceThirdPartyApplication.java
@@ -0,0 +1,20 @@
+package com.caspar.eservicemall.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class EserviceThirdPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserviceThirdPartyApplication.class, args);
+	}
+
+}
+
+
+
+
---
+++ eservice-third-party/src/test/java/com/caspar/eservicemall/thirdparty/EserviceThirdPartyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.caspar.eservicemall.thirdparty;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.alicloud.oss.resource.OssStorageProtocolResolver;
+
+@SpringBootTest
+class EserviceThirdPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
