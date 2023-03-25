ecc36df3c20fc867d4803283d92c07ac5e58494d
chunhongZheng <824394795@qq.com>
2023-03-26 01:06:49 +0800

    新增 eservie-search服务 及elasticsearch学习相关

---
+++ eservice-search/src/main/java/com/caspar/eservicemall/search/EserviceSearchApplication.java
@@ -0,0 +1,16 @@
+package com.caspar.eservicemall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class EserviceSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserviceSearchApplication.class, args);
+	}
+
+}
